60cae04a6d5fa87000d3088aceea7f8adf6b4f5c
Dishwar Raya <drpraya52@gmail.com>
2026-03-08 15:44:12 +0700

    mengembalikan semua folder dan merapikan tailwind

--- modul3/script.js
+++ modul3/script.js
@@ -1,11 +1,1 @@
-function validasiNama() {
-    let inputNama = document.getElementById("nama").value;
-    let pola = /^[a-zA-Z\s]+$/; // Hanya huruf dan spasi
 
-    if(!pola.test(inputNama)) {
-        alert("Nama hanya boleh mengandung huruf dan spasi.");
-        return false;
-    }
-    alert("Nama valid!");
-    return true;
-}
